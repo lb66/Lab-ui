9e0a5b21ee9a9419b871b76295e571382b4ac465
lb66 <807242867@qq.com>
2020-09-25 22:13:51 +0800

    rollup

---
+++ src/lib/index.ts
@@ -0,0 +1,15 @@
+
+export { showDialog as showDialog } from './showDialog';
+export { default as Switch } from './Switch.vue'
+export { default as Button } from './Button.vue'
+export { default as ButtonGroup } from './ButtonGroup.vue'
+export { default as Input } from './input.vue'
+export { default as Toast } from './Toast.vue'
+export { default as Tabs } from './Tabs.vue'
+export { default as Tab } from './Tab.vue'
+export { default as Col } from './Col.vue'
+export { default as Row } from './Row.vue'
+export { default as Collapse } from './Collapse.vue'
+export { default as CollapseItem } from './CollapseItem.vue'
+export { default as Dialog } from './Dialog.vue'
+export { default as Poptip } from './Poptip.vue'
